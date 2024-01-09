150eec960dca7efab57291d7f190df1660428059
Desktop Obsidian <logarithmicpresence@gmail.com>
2024-01-10 05:15:57 +0800

    Minor Fixes

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO LIST
+- Make the front end be able to handle multiple users and update their files here.
+- Separate .json file for cookie handling.
+- Learn SQL or Moongose DB for databasing.
+
+# DIAGRAM
+User 1 ->\
+User 2 ->>== URL params ==> Server
+USer 3 ->/
--- app/webHandler.js
+++ app/webHandler.js
@@ -1,68 +1,53 @@
-console.log("Script Loaded.");
+console.log('Script Loaded.');
 
-var cssProps = document.querySelector(":root");
-var screenList = document.getElementById("screenList");
+var cssProps = document.querySelector(':root');
+var screenList = document.getElementById('screenList');
 var isDarkMode = true;
 
 function main() {
 	const ctrlButtons = {
 		bTheme: [bUpdateWebTheme, {}],
-		bList: [bListGogleCal, {}],
+		bList: [bListGoogleCalendar, {}],
 		bPath: [bInputTaskFile, {}],
 		bMdList: [bListMarkdown, {}],
-		bAddEvents: [bEventsSync, {}]
-
+		bAddEvents: [bEventsSync, {}],
 	};
 	Object.entries(ctrlButtons).forEach(([buttonName, value]) => {
 		value[1] = document.getElementById(buttonName);
 		if (value[1] != null) value[1].onclick = value[0];
 	});
 
-	const buttonsValid = document.getElementsByClassName("pressable");
-	const clickSound = new Audio("../sounds/buttonClick.mp3");
+	const buttonsValid = document.getElementsByClassName('pressable');
+	const clickSound = new Audio('../sounds/buttonClick.mp3');
 	for (const button of buttonsValid) {
 		console.log(button);
-		button.addEventListener("click", () => {
+		button.addEventListener('click', () => {
 			clickSound.play();
 		});
 	}
 
-	const uploadForm = document.querySelector('.upload');
+	const forms = document.getElementsByTagName('form');
+	for (const form of forms) {
+		form.addEventListener('submit', async function (e) {
+			e.preventDefault();
+			console.log(e.target.clientUpload);
+			let file = e.target.clientUpload.files[0];
+			filesend(file, `/api/client/save`);
+		});
+	}
 
-	uploadForm.addEventListener('submit', async function (e){
-		e.preventDefault();
-		console.log(e.target.clientUpload)
-		let file = e.target.clientUpload.files[0];
-		console.log(e.target.clientUpload.files[0])
-		console.table(file)
-		let formData = new FormData()
-		formData.append('file', file);
-		console.table(formData[0])
-		const response = await prowrap(
-			fetch("/api/client/save", {
-				method: "POST",
-				body: formData
-		}));
-		if (response.error) {
-			console.error(response.error);
-		} else {
-			console.log(response.data.json());
-		}
-
-	})
-
-	const buttonsExpandable = document.getElementsByClassName("expandable_button");
+	const buttonsExpandable = document.getElementsByClassName('expandable_button');
 	if (buttonsExpandable) {
 		for (const expandableButton of buttonsExpandable) {
-			expandableButton.addEventListener("click", function () {
-				this.classList.toggle("active");
+			expandableButton.addEventListener('click', function () {
+				this.classList.toggle('active');
 				var content = this.nextElementSibling;
 				if (content.style.maxHeight) {
 					content.style.maxHeight = null;
-					this.parentNode.classList.remove("active_panel");
+					this.parentNode.classList.remove('active_panel');
 				} else {
-					content.style.maxHeight = content.scrollHeight + "px";
-					this.parentNode.classList.add("active_panel");
+					content.style.maxHeight = content.scrollHeight + 'px';
+					this.parentNode.classList.add('active_panel');
 				}
 			});
 		}
@@ -72,229 +57,255 @@ function main() {
 		stickyDetection();
 	};
 
-	window.addEventListener("load", async () => {
+	window.addEventListener('load', async () => {
 		let response = {};
-		await fetch("/api/settings/load")
+		await fetch('/api/settings/load')
 			.then(async (res) => {
 				console.log(res);
-				response = await fetch("/api/settings/load");
+				response = await fetch('/api/settings/load');
 			})
 			.catch(async (err) => {
 				console.error(err);
 				await bUpdateWebTheme().then(async () => {
-					response = await fetch("/api/settings/load");
+					response = await fetch('/api/settings/load');
 				});
 			});
 		console.log(response);
 		const data = await response.json();
-		console.log("Loaded");
+		console.log('Loaded');
 		console.log(data);
-		cssProps.style.setProperty("--bg", data.web["--bg"]);
-		cssProps.style.setProperty("--text", data.web["--text"]);
-		isDarkMode = data.web["--text"] == "white" ? true : false;
-		cssProps.style.getPropertyValue("--bg");
-		const loading_screen = document.getElementById("loading");
+		cssProps.style.setProperty('--bg', data.web['--bg']);
+		cssProps.style.setProperty('--text', data.web['--text']);
+		isDarkMode = data.web['--text'] == 'white' ? true : false;
+		cssProps.style.getPropertyValue('--bg');
+		const loading_screen = document.getElementById('loading');
 		if (loading_screen != null) {
-			loading_screen.classList.add("transparent");
+			loading_screen.classList.add('transparent');
 			setTimeout(() => {
-				loading_screen.classList.add("hidden");
+				loading_screen.classList.add('hidden');
 			}, 250);
 		}
 	});
 }
 
-function stickyDetection() {
-	const navbarYPos = document.getElementById("navbar")?.offsetTop;
-	window.scrollY >= navbarYPos ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
+const stickyDetection = () => {
+	const navbarYPos = document.getElementById('navbar')?.offsetTop;
+	window.scrollY >= navbarYPos ? navbar.classList.add('sticky') : navbar.classList.remove('sticky');
+};
 
-function LoadingBarStatus(status, message) {
-	const loadingBar = document.getElementById("statusPanel")?.getElementsByClassName("loading_bar");
-	const statusSign = document.getElementById("statusSign");
-	const statusMessage = statusSign?.getElementsByTagName("b");
-	statusSign.innerHTML = "<b>Status:</b> " + message;
+const LoadingBarStatus = async (status, message) => {
+	const loadingBar = document.getElementById('statusPanel')?.getElementsByClassName('loading_bar');
+	const statusSign = document.getElementById('statusSign');
+	const statusMessage = statusSign?.getElementsByTagName('b');
+	statusSign.innerHTML = '<b>Status:</b> ' + message;
 	switch (status) {
-		case "fail":
-		case "failure":
-			loadingBar[0].classList.add("hidden");
-			statusMessage[0].style.setProperty("color", "red");
+		case 'fail':
+		case 'failure':
+			loadingBar[0].classList.add('hidden');
+			statusMessage[0].style.setProperty('color', 'red');
 			break;
-		case "fetch":
-			loadingBar[0].classList.remove("hidden");
-			statusMessage[0].style.setProperty("color", "yellow");
+		case 'fetch':
+			loadingBar[0].classList.remove('hidden');
+			statusMessage[0].style.setProperty('color', 'yellow');
 			break;
-		case "idle":
-		case "ok":
+		case 'idle':
+		case 'ok':
 		default:
-			loadingBar[0].classList.add("hidden");
-			statusMessage[0].style.setProperty("color", "green");
+			loadingBar[0].classList.add('hidden');
+			statusMessage[0].style.setProperty('color', 'green');
 			break;
 	}
-}
+};
 
 const idleStatus = async () => {
-	const message = "Idle";
+	const message = 'Idle';
 	setTimeout(() => {
-		LoadingBarStatus("green", message);
+		LoadingBarStatus('green', message);
 	}, 2000);
 };
 
-function bUpdateWebTheme() {
+const bUpdateWebTheme = () => {
 	if (isDarkMode) {
-		cssProps.style.setProperty("--bg", "white");
-		cssProps.style.setProperty("--text", "#080808");
+		cssProps.style.setProperty('--bg', 'white');
+		cssProps.style.setProperty('--text', '#080808');
 		isDarkMode = false;
 	} else {
-		cssProps.style.setProperty("--bg", "#080808");
-		cssProps.style.setProperty("--text", "white");
+		cssProps.style.setProperty('--bg', '#080808');
+		cssProps.style.setProperty('--text', 'white');
 		isDarkMode = true;
 	}
 	var docProperties = getComputedStyle(cssProps);
 	var colorObj = {
 		web: {
-			"--bg": docProperties.getPropertyValue("--bg"),
-			"--text": docProperties.getPropertyValue("--text"),
+			'--bg': docProperties.getPropertyValue('--bg'),
+			'--text': docProperties.getPropertyValue('--text'),
 		},
 	};
-	fetch("/api/settings/save", {
-		method: "POST",
+	fetch('/api/settings/save', {
+		method: 'POST',
 		headers: {
-			"Content-Type": "application/json",
+			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(colorObj),
 	});
-}
+};
 
-async function bListGogleCal() {
-	LoadingBarStatus("fetch", "Fetching...");
+const bListGoogleCalendar = async () => {
+	LoadingBarStatus('fetch', 'Fetching...');
 
-	let data = {};
-	let events = "Tasks\n";
-	const response = await prowrap(fetch("/api/events/list/googleCalendar"))
+	let data = {},
+		events = 'Tasks\n';
+
+	const response = await prowrap(fetch('/api/events/list/googleCalendar'));
 
 	if (response.error) {
-		data = { events: "" };
-		LoadingBarStatus("failure", "No Events Detected");
+		data = {events: ''};
+		LoadingBarStatus('failure', 'No Events Detected');
 	} else {
-		console.log("Success", data.events);
+		console.log('Success', data.events);
 		data = await response.data.json();
-		console.log(data)
-		if (data.events == "") return LoadingBarStatus("failure", "No Events Detected");
-		LoadingBarStatus("ok", "Idle");
+		console.log(data);
+		if (data.events == '') return LoadingBarStatus('failure', 'No Events Detected');
+		LoadingBarStatus('ok', 'Idle');
 		for (var i = 0; i < Object.keys(data.events).length; i++) {
-			console.log(`${data.events[i]["item"]}) ${data.events[i]["date"]} is ${data.events[i]["event"]}`);
-			events += `${Number(data.events[i]["item"]) + 1}) ${data.events[i]["date"]} is ${data.events[i]["event"]}\n`;
+			console.log(`${data.events[i]['item']}) ${data.events[i]['date']} is ${data.events[i]['event']}`);
+			events += `${Number(data.events[i]['item']) + 1}) ${data.events[i]['date']} is ${data.events[i]['event']}\n`;
 		}
-		screenList.innerHTML = events;
 	}
-
-	if (data.events == "") screenList.innerHTML = `None`;
+	screenList.innerHTML = data.events == '' ? `None` : events;
 
 	return idleStatus();
-}
+};
 
-async function bListMarkdown() {
-	LoadingBarStatus("fetch", "Fetching...");
-	var innerString = "Tasks:\n";
+const bListMarkdown = async () => {
+	LoadingBarStatus('fetch', 'Fetching...');
+	var innerString = 'Tasks:\n';
 	var data = {};
 
-	await fetch("/api/events/list/markdown")
+	await fetch('/api/events/list/markdown')
 		.then(async (response) => {
 			data = await response.json();
-			console.log("Success", data["tasks"]);
-			for (var i = 0; i < Object.keys(data["tasks"]).length; i++) {
-				console.log(` ${i.toString().padStart(2, "0")}) ${data["tasks"][i]}`);
-				innerString += `${i.toString().padStart(2, "0")}) ${data["tasks"][i]}\n`;
+			console.log('Success', data['tasks']);
+			for (var i = 0; i < Object.keys(data['tasks']).length; i++) {
+				console.log(` ${i.toString().padStart(2, '0')}) ${data['tasks'][i]}`);
+				innerString += `${i.toString().padStart(2, '0')}) ${data['tasks'][i]}\n`;
 				screenList.innerHTML = innerString;
 			}
-			LoadingBarStatus("ok", "Idle");
+			LoadingBarStatus('ok', 'Idle');
 			console.log(data);
 		})
 		.catch((err) => {
 			data = {
-				events: "",
+				events: '',
 			};
-			LoadingBarStatus("failure", "No Events Detected in Markdown List");
+			LoadingBarStatus('failure', 'No Events Detected in Markdown List');
 		});
 
-	if (data.events == "") {
+	if (data.events == '') {
 		screenList.innerHTML = `None`;
 	}
 
 	idleStatus();
-}
+};
 
-async function bInputTaskFile() {
-	let inputMarkdownAddress = document.getElementById("bPathText");
-	LoadingBarStatus("fetch", "Fetching...");
-	console.log("bInputTaskFile");
-	var markData = { markdown_path: inputMarkdownAddress.value };
-	if (!confirm(`Do you want to save the following Data? Address:${markData["markdown_path"]}`)) {
-		LoadingBarStatus("failure", "Data not submitted");
-		return idleStatus();	
-	}
-	const response = await prowrap(fetch("/api/settings/save", {
-		method: "POST",
-		headers: { "Content-Type": "application/json"},
-		body: JSON.stringify(markData),
-	}))
-	if (response.error) {
-		LoadingBarStatus("failure", "Data failed to be saved");
-		console.error(err);
-		return idleStatus();	
-	}
-	LoadingBarStatus("ok", "Data saved");
-	return idleStatus();	
-}
-
-async function bEventsSync() {
-	LoadingBarStatus("fetch", "Fetching...");
-
-	const response = await prowrap(fetch("/api/events/sync"))
-	if (response.error){
-		LoadingBarStatus("failure", err);
+const bInputTaskFile = async () => {
+	let inputMarkdownAddress = document.getElementById('bPathText');
+	LoadingBarStatus('fetch', 'Fetching...');
+	console.log('bInputTaskFile');
+	var markData = {markdown_path: inputMarkdownAddress.value};
+	if (!confirm(`Do you want to save the following Data? Address:${markData['markdown_path']}`)) {
+		LoadingBarStatus('failure', 'Data not submitted');
 		return idleStatus();
-	} 
-	let eventsAdded = await response.data.json()
-	LoadingBarStatus("ok", `Synced Events:`);
+	}
+	const response = await prowrap(
+		fetch('/api/settings/save', {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(markData),
+		})
+	);
+	if (response.error) {
+		LoadingBarStatus('failure', 'Data failed to be saved');
+		console.error(err);
+		return idleStatus();
+	}
+	LoadingBarStatus('ok', 'Data saved');
+	return idleStatus();
+};
+
+const bEventsSync = async () => {
+	LoadingBarStatus('fetch', 'Fetching...');
+
+	const response = await prowrap(fetch('/api/events/sync'));
+	if (response.error) {
+		LoadingBarStatus('failure', err);
+		return idleStatus();
+	}
+	let eventsAdded = await response.data.json();
+	LoadingBarStatus('ok', `Synced Events:`);
 	screenList.innerHTML = JSON.stringify(eventsAdded);
-	
 
 	idleStatus();
-}
+};
 
-async function bClientSecrets() {
-	LoadingBarStatus("fetch", "Fetching...");
-	let inputJSON = document.getElementById("bPathText");
-	var client_secrets = { value: inputJSON.files[0] };
+const bClientSecrets = async () => {
+	LoadingBarStatus('fetch', 'Fetching...');
+	let inputJSON = document.getElementById('bPathText');
+	var client_secrets = {value: inputJSON.files[0]};
+
 	console.log(client_secrets.value);
 	if (!confirm(`Do you want to save the following Data? Address:\n${client_secrets.value}`)) {
-		LoadingBarStatus("failure", "User did not send the Data")
+		LoadingBarStatus('failure', 'User did not send the Data');
 		idleStatus();
 		return;
-	};
+	}
 	const response = await prowrap(
-		fetch("/api/client/save", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(client_secrets)
-	}));
-    console.log(response)
+		fetch('/api/client/save', {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(client_secrets),
+		})
+	);
+	console.log(response);
+};
 
-}
+const filesend = async (file, url) => {
+	const fileReader = new FileReader();
+
+	fileReader.readAsArrayBuffer(file);
+	fileReader.onload = async (event) => {
+		const fileContent = event.target.result;
+		console.log(fileContent)
+		const CHUNK_SIZE = 8000; //Kilobytes
+		const totalChunks = fileContent.byteLength / CHUNK_SIZE;
+		for (let chunk = 0; chunk < totalChunks + 1; chunk++) {
+			
+			let DATACHUNK = fileContent.slice(chunk * CHUNK_SIZE, (chunk + 1) * CHUNK_SIZE);
+			console.log(DATACHUNK)
+			await fetch(url + '?filename=' + file.name, {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/octet-stream',
+					'content-length': DATACHUNK.length,
+				},
+				body: DATACHUNK,
+			});
+		}
+	};
+};
 
 const prowrap = async (promise, isLogging = false) => {
 	try {
 		const result = await Promise.allSettled([promise]);
-		if (result.find((res) => res.status === "fulfilled")) {
-			return { data: result.find((res) => res.status === "fulfilled")?.value, error: null };
-		}
-		const error = result.find((res) => res.status === "rejected")?.reason;
+		if (result.find((res) => res.status === 'fulfilled'))
+			return {data: result.find((res) => res.status === 'fulfilled')?.value, error: null};
+
+		const error = result.find((res) => res.status === 'rejected')?.reason;
 		if (isLogging) console.log(result);
-		return { data: null, error: error };
+		return {data: null, error: error};
 	} catch (error) {
 		if (isLogging) console.log(error);
-		return { data: null, error: error };
+		return {data: null, error: error};
 	}
 };
 
